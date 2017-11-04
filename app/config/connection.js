b95d86865e662a0a3d82402af25a8eef2ed501b1
Prashan Welipitiya <prshn.weli@gmail.com>
2017-11-03 19:05:53 -0700

    didnt

--- app/config/connection.js
+++ app/config/connection.js
@@ -6,7 +6,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("sequelize_work", "root", "karpal3473", {
+var sequelize = new Sequelize("sequelize_work", "root", "password", {
   host: "localhost",
   dialect: "mysql",
   pool: {
